cb443daa8cd0ecb5f48e14462932353fe95f86bb
Mohammad Noruzi <mohammadnorozy77@gmail.com>
2023-07-25 13:09:43 +0330

    add all

---
+++ routes/authRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const {
+  test,
+  signup,
+  loginUser,
+  getProfile,
+} = require("../controllers/authController");
+
+// mid
+
+router.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+router.get("/", test);
+router.post("/signup", signup);
+router.post("/login", loginUser);
+router.get("/profile", getProfile);
+
+module.exports = router;
